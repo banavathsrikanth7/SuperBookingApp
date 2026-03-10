8e66b1500fbe45beb51358c742357c051c4df770
arnabdas9039 <arnabdas.9039@gmail.com>
2026-03-11 04:23:43 +0530

    Models, Migrations, Admin, Auth, 1 Serializer

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -51,11 +51,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "api",
+    "authentication",
     "booking",
     "content",
     "user",
-    "rest_framework",
     "corsheaders",
 ]
 
