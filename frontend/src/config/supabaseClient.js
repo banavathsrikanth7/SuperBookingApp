68e96def62b91c90ca99df0b6a19c61af61cd3b2
banavathsrikanth7 <srikanth10.24@kgpian.iitkgp.ac.in>
2026-06-14 12:35:00 +0530

    i have created two pages state and city page and  added the api calls to fetch the data from the   backend and display it on the frontend.    I have also added the routing for the new pages    in the App.jsx file.

--- frontend/src/config/supabaseClient.js
+++ frontend/src/config/supabaseClient.js
@@ -6,5 +6,6 @@ const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 if (!supabaseUrl || !supabaseAnonKey) {
   console.warn('Supabase URL or Anon Key is missing from environment variables.');
 }
-
+console.log("SUPABASE URL =", supabaseUrl);
+console.log("SUPABASE KEY =", supabaseAnonKey);
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
